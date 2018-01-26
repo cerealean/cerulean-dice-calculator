5f1910bcac5fe36f2c2125f4550ec8b432bafcd4
Michael Crawford <cerealean@me.com>
2018-01-26 14:38:43 -0600

    Added dice roller class and basics to start project

---
+++ src/app/dice-roller/dice-roller.ts
@@ -0,0 +1,13 @@
+export class DiceRoller {
+    minimumDiceRoll:number = 1;
+
+    public rollDice(numberOfSides:number) {
+        return this.getRandomIntInclusive(this.minimumDiceRoll, numberOfSides);
+    }
+
+    private getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+}
