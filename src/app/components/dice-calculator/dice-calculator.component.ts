a8f16d9e10f0a0aaa95961e991496beeee9c6f7f
Michael Crawford <cerealean@me.com>
2018-02-05 18:03:12 -0600

    Have all buttons except equals button adding to string calculator

--- src/app/components/dice-calculator/dice-calculator.component.ts
+++ src/app/components/dice-calculator/dice-calculator.component.ts
@@ -4,11 +4,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dice-calculator.component.html',
   styleUrls: ['./dice-calculator.component.scss']
 })
-export class DiceCalculatorComponent implements OnInit {
+export class DiceCalculatorComponent {
+
+  specialCharacters:string[] = ["D","/","+","-","x"];
+
+  stringToCalculate:string = "";
 
   constructor() { }
 
-  ngOnInit() {
+  public AddCharacter(char:string):void{
+    if(this.IsSpecialCharacter(char) && this.IsSpecialCharacter(this.stringToCalculate.slice(-1))){
+      return;
+    }
+    this.stringToCalculate+=char;
   }
 
+  public Clear():void{
+    this.stringToCalculate = "";
+  }
+
+  private IsSpecialCharacter(char:string):boolean{
+    return this.specialCharacters.indexOf(char) != -1;
+  }
 }
