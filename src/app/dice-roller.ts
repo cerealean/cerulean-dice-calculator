d4baa0fc4acd385a4c237c4a95693ac04f537976
Michael Crawford <cerealean@me.com>
2018-02-05 22:52:07 -0600

    Was playing with crypto. Need more research

--- src/app/dice-roller.ts
+++ src/app/dice-roller.ts
@@ -8,6 +8,7 @@ export class DiceRoller {
     private getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
+        
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
