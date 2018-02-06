1ccde31b8b32b0430a383e8993193c7b3527f861
Michael Crawford <cerealean@me.com>
2018-02-05 18:20:41 -0600

    Created string dice calculator

---
+++ src/app/string-dice-calculator/string-dice-calculator.spec.ts
@@ -0,0 +1,7 @@
+import { StringDiceCalculator } from './string-dice-calculator';
+
+describe('StringDiceCalculator', () => {
+  it('should create an instance', () => {
+    expect(new StringDiceCalculator()).toBeTruthy();
+  });
+});
