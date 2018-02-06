4b597e0e5f2167c6036b35b0540b238f0d95e52a
Michael Crawford <cerealean@me.com>
2018-02-05 23:08:23 -0600

    Made equals sign repeat last calculation

--- src/app/components/dice-calculator/dice-calculator.component.ts
+++ src/app/components/dice-calculator/dice-calculator.component.ts
@@ -6,31 +6,38 @@ import { StringDiceCalculator } from '../../string-dice-calculator/string-dice-c
   styleUrls: ['./dice-calculator.component.scss']
 })
 export class DiceCalculatorComponent {
-  specialCharacters:string[] = ["D","/","+","-","x"];
-  stringToCalculate:string = "";
-  equalsKeyHit:boolean = false;
+  specialCharacters: string[] = ["D", "/", "+", "-", "x"];
+  stringToCalculate: string = "";
+  previousStringToCalculate: string = "";
+  equalsKeyHit: boolean = false;
 
-  public AddCharacter(char:string):void{
-    if(this.equalsKeyHit === true){
+  public AddCharacter(char: string): void {
+    if (this.equalsKeyHit === true) {
       this.equalsKeyHit = false;
       this.stringToCalculate = "";
     }
-    if(this.IsSpecialCharacter(char) && this.IsSpecialCharacter(this.stringToCalculate.slice(-1))){
+    if (this.IsSpecialCharacter(char) && this.IsSpecialCharacter(this.stringToCalculate.slice(-1))) {
       return;
     }
-    this.stringToCalculate+=char;
+    this.stringToCalculate += char;
   }
 
-  public Clear():void{
+  public Clear(): void {
     this.stringToCalculate = "";
   }
 
-  public Calculate():void{
-    this.stringToCalculate = new StringDiceCalculator().CalculateFromString(this.stringToCalculate).toString();
-    this.equalsKeyHit = true;
+  public Calculate(): void {
+    if (this.equalsKeyHit === true) {
+      this.stringToCalculate = new StringDiceCalculator().CalculateFromString(this.previousStringToCalculate).toString();
+    }
+    else {
+      this.previousStringToCalculate = this.stringToCalculate;
+      this.stringToCalculate = new StringDiceCalculator().CalculateFromString(this.stringToCalculate).toString();
+      this.equalsKeyHit = true;
+    }
   }
 
-  private IsSpecialCharacter(char:string):boolean{
+  private IsSpecialCharacter(char: string): boolean {
     return this.specialCharacters.indexOf(char) != -1;
   }
 }
