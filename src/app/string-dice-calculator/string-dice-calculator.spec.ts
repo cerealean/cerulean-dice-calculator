a2a72800e829c21f4d1295002be227a0a26f46d4
Michael Crawford <cerealean@me.com>
2018-02-05 22:21:41 -0600

    Added tests for string dice calculator and implemented more of the class. Needs to be cleaned up.

--- src/app/string-dice-calculator/string-dice-calculator.spec.ts
+++ src/app/string-dice-calculator/string-dice-calculator.spec.ts
@@ -1,7 +1,140 @@
 import { StringDiceCalculator } from './string-dice-calculator';
 
 describe('StringDiceCalculator', () => {
+  let classUnderTest:StringDiceCalculator;
+
+  beforeEach(() => {
+    classUnderTest = new StringDiceCalculator();
+  });
+
   it('should create an instance', () => {
     expect(new StringDiceCalculator()).toBeTruthy();
+  });
+  
+  describe('CalculateFromString', () => {
+    it('should return 0 from an empty string', () => {
+      const givenValue = "";
+      const expectedValue = 0;
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBe(expectedValue);
+    });
+    
+    it('should return 1 if given 1', () => {
+      const givenValue = "1";
+      const expectedValue = 1;
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBe(expectedValue);
+    });
+
+    it('should return 135 if given 135', () => {
+      const givenValue = "135";
+      const expectedValue = 135;
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBe(expectedValue);
+    });
+
+    it('should return 4 if given 2+2', () => {
+      const givenValue = "2+2";
+      const expectedValue = 4;
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBe(expectedValue);
+    });
+
+    it('should return a number between 4 and 6 if given 2+2D2', () => {
+      const givenValue = "2+2D2";
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBeGreaterThanOrEqual(4);
+      expect(actual).toBeLessThanOrEqual(6);
+    });  
+
+    it('should return a number between 4 and 22 if given 2+2D10', () => {
+      const givenValue = "2+2D10";
+      const smallestAllowedValue = 4;
+      const largestAllowedValue = 22;
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBeGreaterThanOrEqual(smallestAllowedValue);
+      expect(actual).toBeLessThanOrEqual(largestAllowedValue);
+    });  
+
+    it('should return a number between 5 and 40 if given 5D8', () => {
+      const givenValue = "5D8";
+      const smallestAllowedValue = 5;
+      const largestAllowedValue = 40;
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBeGreaterThanOrEqual(smallestAllowedValue);
+      expect(actual).toBeLessThanOrEqual(largestAllowedValue);
+    });  
+
+    it('should return -3 when given -2-1', () => {
+      const givenValue = "-2 - 1";
+      const expectedValue = -3;
+
+      const actual = classUnderTest.CalculateFromString(givenValue);
+
+      expect(actual).toBe(expectedValue);
+    });
+
+    it('should throw an error if 2D2D is given', () => {
+      const givenValue = "2D2D";
+
+      expect(() => classUnderTest.CalculateFromString(givenValue))
+        .toThrowError();
+    });
+
+    it('should throw an error if 2D2DDD is given', () => {
+      const givenValue = "2D2DDD";
+
+      expect(() => classUnderTest.CalculateFromString(givenValue))
+        .toThrowError();
+    });
+
+    it('should throw an error if 2+4D+', () => {
+      const givenValue = "2+4D+";
+
+      expect(() => classUnderTest.CalculateFromString(givenValue))
+        .toThrowError();
+    });
+
+    it('should throw an error if 2D is given', () => {
+      const givenValue = "2D";
+
+      expect(() => classUnderTest.CalculateFromString(givenValue))
+        .toThrowError();
+    });
+    
+    it('should throw an error if D is given', () => {
+      const givenValue = "D";
+
+      expect(() => classUnderTest.CalculateFromString(givenValue))
+        .toThrowError();
+    });
+    
+    it('should throw an error if D2 is given', () => {
+      const givenValue = "D2";
+
+      expect(() => classUnderTest.CalculateFromString(givenValue))
+        .toThrowError();
+    });
+    
+    it('should throw an error if x2 is given', () => {
+      const givenValue = "x2";
+
+      expect(() => classUnderTest.CalculateFromString(givenValue))
+        .toThrowError();
+    });
   });
 });
--- src/app/string-dice-calculator/string-dice-calculator.ts
+++ src/app/string-dice-calculator/string-dice-calculator.ts
@@ -1,2 +1,84 @@
+import { DiceRoller } from "../dice-roller";
+
 export class StringDiceCalculator {
+    diceCharacter: string = "D";
+    diceRoller: DiceRoller = new DiceRoller();
+    operators = new RegExp(/\+|-|\/|x/);
+
+    public CalculateFromString(input: string): number {
+        if (!input) {
+            return 0;
+        }
+        let startsNegative = input.substr(0,1) === "-";
+        if(startsNegative){
+            input = input.substr(1);
+        }
+        this.ThrowErrorIfInvalidStartOrEndOfStringGiven(input);
+        let numbersToCalculate = input.split(this.operators);
+        const onlyOperators = input.match(this.operators);
+        const convertedNumbers: number[] = this.ConvertDiceValuesToNumbers(numbersToCalculate);
+        let convertedString = "";
+
+        if (onlyOperators) {
+            if(startsNegative)
+            {
+                convertedString += "-";
+            }
+            for (let index = 0; index < onlyOperators.length; index++) {
+                convertedString += String(convertedNumbers[index]) + onlyOperators[index];
+            }
+            convertedString += convertedNumbers[convertedNumbers.length - 1];
+
+            return eval(convertedString);
+        }
+        else {
+            return convertedNumbers[0];
+        }
+    }
+
+    private ThrowErrorIfInvalidStartOrEndOfStringGiven(input: string) {
+        const lastCharacter = input.slice(-1);
+        const firstCharacter = input.slice(0,1);
+        const disallowedFirstCharacters = new RegExp(/\/|x/);
+        if (lastCharacter.match(this.operators) || lastCharacter == this.diceCharacter) {
+            throw Error("Last character cannot be an operator or \"D\"");
+        }
+        if (firstCharacter.match(disallowedFirstCharacters) || firstCharacter == this.diceCharacter) {
+            throw Error("First character cannot be an operator or \"D\"");
+        }
+    }
+
+    private ConvertDiceValuesToNumbers(values: string[]): number[] {
+        let convertedValues: number[] = [];
+
+        for (let value of values) {
+            let convertedValue: number;
+            if (value.indexOf(this.diceCharacter) != -1) {
+                const total = this.ConvertDiceValueToNumber(value);
+                convertedValues.push(total);
+            }
+            else {
+                convertedValues.push(Number(value));
+            }
+        }
+
+        return convertedValues;
+    }
+
+    private ConvertDiceValueToNumber(value: string): number {
+        let splitValue = value.split(this.diceCharacter);
+        const numberOfDiceRolls = Number(splitValue[0]);
+        const numberOfSides = Number(splitValue[1]);
+        let total = 0;
+        if (splitValue.length > 2) {
+            throw Error(value + " is an invalid value");
+        }
+        for (let index = 0; index < numberOfDiceRolls; index++) {
+            const rolledValue: number = this.diceRoller.rollDice(numberOfSides);
+            total += rolledValue;
+        }
+
+        return total;
+    }
+
 }
