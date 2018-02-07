e74f2b3016ce8b99b2e613fd58835160a743a144
Michael Crawford <cerealean@me.com>
2018-02-06 20:37:38 -0600

    Using new random generator to roll dice

--- src/app/dice-roller.ts
+++ src/app/dice-roller.ts
@@ -1,14 +1,13 @@
+import * as Random from 'random-js';
 export class DiceRoller {
     minimumDiceRoll:number = 1;
+    randomGenerator:any = new Random(Random.engines.mt19937().autoSeed());
 
-    public rollDice(numberOfSides:number) {
+    public rollDice(numberOfSides:number):number {
         return this.getRandomIntInclusive(this.minimumDiceRoll, numberOfSides);
     }
 
-    private getRandomIntInclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    private getRandomIntInclusive(min, max):number {
+        return this.randomGenerator.integer(min, max);
     }
 }
