688e25c29b365cf0351bf29d06d6183fb28a8143
Michael Crawford <cerealean@me.com>
2018-02-08 21:31:49 -0600

    Not really sure

--- src/app/components/dice-calculator/dice-calculator.component.ts
+++ src/app/components/dice-calculator/dice-calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { StringDiceCalculator } from '../../string-dice-calculator/string-dice-calculator';
 
 @Component({
@@ -13,16 +13,23 @@ export class DiceCalculatorComponent {
   equalsKeyHit: boolean = true;
 
   public AddCharacter(char: string): void {
-    if (this.equalsKeyHit === true) {
+    if(this.IsSpecialCharacter(char) && this.equalsKeyHit === true){
+      this.output = this.stringToCalculate;
+      this.equalsKeyHit = false;
+    }
+    else if (this.equalsKeyHit === true) {
       this.equalsKeyHit = false;
       this.stringToCalculate = "";
       this.output = "";
     }
-    if (this.IsSpecialCharacter(char) && this.IsSpecialCharacter(this.stringToCalculate.slice(-1))) {
+    else if (this.IsSpecialCharacter(char) && this.IsSpecialCharacter(this.stringToCalculate.slice(-1))) {
       return;
     }
     this.stringToCalculate += char;
     this.output += char;
+    this.equalsKeyHit = false;
+    console.log("Output: " + this.output);
+    console.log("stringtocalc: " + this.stringToCalculate);
   }
 
   public Clear(): void {
@@ -39,13 +46,30 @@ export class DiceCalculatorComponent {
     }
     else if (this.equalsKeyHit === true && this.previousStringToCalculate) {
       result = new StringDiceCalculator().CalculateFromString(this.previousStringToCalculate);
-      this.output = this.stringToCalculate + "=" + result;
+      this.output = result.toString();
     }
     else {
       this.previousStringToCalculate = this.stringToCalculate;
       result = new StringDiceCalculator().CalculateFromString(this.stringToCalculate);
-      this.output = this.stringToCalculate + "=" + result;
+      this.output = result.toString();
       this.equalsKeyHit = true;
+    }
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  private HandleKeyboardInput(event:KeyboardEvent){
+    const allowedCharacters = /[0-9]|(d|D|\.|\+|\-|\*|\/)/;
+
+    if(allowedCharacters.test(event.key)){
+      if(event.key === "d"){
+        this.AddCharacter("D");
+      }
+      else{
+        this.AddCharacter(event.key);
+      }
+    }
+    else if(event.which === 13){
+      this.Calculate();
     }
   }
 
