51d68463a8002029268978d9b3bf0669fedfa9f2
Michael Crawford <cerealean@me.com>
2018-08-30 15:32:28 -0500

    Fixed import of random-js

--- src/app/dice-roller.ts
+++ src/app/dice-roller.ts
@@ -1,7 +1,8 @@
 import * as randomjs from 'random-js';
+
 export class DiceRoller {
     minimumDiceRoll:number = 1;
-    private engine = randomjs.Random.engines.mt19937().autoSeed();
+    private engine = randomjs.engines.mt19937().autoSeed();
 
     public rollDie(numberOfSides:number):number {
         return randomjs.Random.die(numberOfSides)(this.engine);
