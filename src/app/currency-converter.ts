3bd13109fbb036226d7f841d611d17c0708c8b59
Michael Crawford <cerealean@me.com>
2018-09-10 14:20:25 -0500

    Finished writing currency conversions

--- src/app/currency-converter.ts
+++ src/app/currency-converter.ts
@@ -3,7 +3,7 @@ import { CurrencyTypes } from "./enums/currency-type";
 export class CurrencyConverter {
     private value: number;
     private type: CurrencyTypes;
-    private conversionRates: Map<CurrencyTypes, Map<CurrencyTypes, number>> = new Map([
+    private static conversionRates: Map<CurrencyTypes, Map<CurrencyTypes, number>> = new Map([
         [CurrencyTypes.Copper, new Map<CurrencyTypes, number>([
             [CurrencyTypes.Copper, 1],
             [CurrencyTypes.Silver, 10],
@@ -12,7 +12,32 @@ export class CurrencyConverter {
             [CurrencyTypes.Silver, 1000],
         ])],
         [CurrencyTypes.Silver, new Map<CurrencyTypes, number>([
-            
+            [CurrencyTypes.Copper, 1/10],
+            [CurrencyTypes.Silver, 1],
+            [CurrencyTypes.Electrum, 5],
+            [CurrencyTypes.Gold, 10],
+            [CurrencyTypes.Silver, 100],
+        ])],
+        [CurrencyTypes.Electrum, new Map<CurrencyTypes, number>([
+            [CurrencyTypes.Copper, 1/50],
+            [CurrencyTypes.Silver, 1/5],
+            [CurrencyTypes.Electrum, 1],
+            [CurrencyTypes.Gold, 2],
+            [CurrencyTypes.Silver, 20],
+        ])],
+        [CurrencyTypes.Gold, new Map<CurrencyTypes, number>([
+            [CurrencyTypes.Copper, 1/100],
+            [CurrencyTypes.Silver, 1/10],
+            [CurrencyTypes.Electrum, 1/2],
+            [CurrencyTypes.Gold, 1],
+            [CurrencyTypes.Silver, 10],
+        ])],
+        [CurrencyTypes.Platinum, new Map<CurrencyTypes, number>([
+            [CurrencyTypes.Copper, 1/1000],
+            [CurrencyTypes.Silver, 1/100],
+            [CurrencyTypes.Electrum, 1/20],
+            [CurrencyTypes.Gold, 1/10],
+            [CurrencyTypes.Silver, 1],
         ])]
     ]);
 
